6e4a39b8f46441c9fce1ee512cd4ca16c43c6043
Alexander von Below <alexander.von_below@telekom.de>
2019-07-15 12:09:06 +0200

    Working, but too much

--- TextViewTest/ViewController.swift
+++ TextViewTest/ViewController.swift
@@ -8,6 +8,50 @@
 
 import UIKit
 
+class TestTextField: UITextField {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print ("Boom");
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let menu = UIMenuController.shared
+        if self.isFirstResponder {
+            self.isHighlighted = false
+            menu.setMenuVisible(false, animated: true)
+            menu.update()
+            self.resignFirstResponder()
+        } else if self.becomeFirstResponder() {
+            menu.setTargetRect(self.bounds, in: self)
+            menu.setMenuVisible(true, animated: true)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.isUserInteractionEnabled = true
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        return self
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(UIResponder.copy(_:)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    override func copy(_ sender: Any?) {
+        print ("Foomp")
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
